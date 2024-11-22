a9b547cf30f10631cfc9b2572a9fc724f90cd7fe
unknown <lromaniellopiccino@gmail.com>
2024-11-22 15:35:14 -0300

    Configuracion Spring security

---
+++ PruebasMicroservice/src/main/java/fran/fourcade/pruebasmicroservice/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package fran.fourcade.pruebasmicroservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.POST, "/api/pruebas")
+                .hasRole("Empleado")
+
+                .requestMatchers(HttpMethod.POST, "/api/posiciones")
+                .hasRole("Vehículo")
+
+                .requestMatchers(HttpMethod.GET, "/api/reportes/incidentes")
+                .hasRole("Admin")
+        ).oauth2ResourceServer(oauth2 -> oauth2. jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        var grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+}
